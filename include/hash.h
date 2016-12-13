222f6a2397c0b92526af847449b787730ef247e6
Mario Saldinger <mariosaldinger@gmail.com>
2016-12-13 16:21:15 +0200

    Add Hash prototypes and refactored Makefile to comply with the Assignment

---
+++ include/hash.h
@@ -0,0 +1,24 @@
+#ifndef HASH_H_
+#define HASH_H_
+
+#include "record.h"
+
+struct HT_info {
+  char attr_type;
+  int file_desc;
+  int num_buckets;
+  size_t attr_length;
+  char* attr_name;
+};
+typedef struct HT_info HT_info;
+
+int HT_CreateIndex(const char* filename, char attr_type, const char* attr_name,
+                   size_t attr_length, int num_buckets);
+
+struct HT_info* HT_OpenIndex(const char* filename);
+int HT_CloseIndex(struct HT_info* hash);
+
+int HT_InsertEntry(struct HT_info hash, struct Record record);
+int HT_GetAllEntries(struct HT_info hash, void* value);
+
+#endif  // HASH_H_
