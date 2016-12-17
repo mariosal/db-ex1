6f432144b2ea03ff407c963968b35bb20bf54b17
Alexandros Kalimeris <kalimerisalx@gmail.com>
2016-12-17 22:09:03 +0200

    Exhash.c:Add function and struct definitions

--- include/exhash.h
+++ include/exhash.h
@@ -3,4 +3,22 @@
 
 #include "record.h"
 
+struct ET_info {
+  char attr_type;
+  int file_desc;
+  int depth;
+  size_t attr_length;
+  char* attr_name;
+};
+typedef struct ET_info ET_info;
+
+int ET_CreateIndex(const char* filename, const char* attr_name, char attr_type,
+                   size_t attr_length, int num_buckets);
+
+struct ET_info* ET_OpenIndex(const char* filename);
+int ET_CloseIndex(struct ET_info* hash);
+
+int ET_InsertEntry(struct ET_info* hash, struct Record record);
+int ET_GetAllEntries(struct ET_info hash, const void* value);
+
 #endif  // EXHASH_H_
